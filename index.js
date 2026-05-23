2bf5b210c5e8d6af18be5cb0eea9eeaf64a30b00
Akash Bhangi <bhangiakasha@gmail.com>
2026-05-23 13:15:27 +0530

    feat: add listing CRUD views and layout structure

--- index.js
+++ index.js
@@ -2,20 +2,69 @@ const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
 const port = 4000;
+const Listing = require("./models/listing.js");
+const path = require("path");
+const methodOverride = require("method-override");
+const ejsMate = require("ejs-mate");
+
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
+app.engine("ejs", ejsMate);
+
+app.use(methodOverride("_method"));
+app.use(express.urlencoded({ extended: true }));
+async function main() {
+    await mongoose.connect("mongodb://localhost:27017/airbnb-mern-clone");
+}
+
+main().then(() => console.log("Database Connected")).catch(err => console.log(err));
+
 
 app.get("/", (req, res) => {
     res.send("Airbnb clone");
 });
 
+app.get("/listings", async (req, res) => {
+    const allListings = await Listing.find({});
+    res.render("listings/index.ejs", { allListings });
+});
+
+app.get("/listings/new", (req, res) => {
+    res.render("listings/new.ejs");
+})
+
+app.post("/listings", async (req, res) => {
+    const newListing = req.body;
+    await Listing.create({ ...newListing });
+    res.redirect("/listings");
+})
+
+app.get("/listings/:id", async (req, res) => {
+    const { id } = req.params;
+    const listing = await Listing.findById(id);
+    res.render("listings/show.ejs", { listing });
+})
+
+app.get("/listings/:id/edit", async (req, res) => {
+    const { id } = req.params;
+    const listing = await Listing.findById(id);
+    res.render("listings/edit.ejs", { listing });
+})
+
+app.put("/listings/:id", async (req, res) => {
+    const { id } = req.params;
+    const newListing = req.body;
+    await Listing.findByIdAndUpdate(id, { ...newListing });
+    res.redirect(`/listings/${id}`);
+})
 
 
-async function main() {
-    await mongoose.connect("mongodb://localhost:27017/airbnb-mern-clone");
-}
 
-main().then(() => console.log("Database Connected")).catch(err => console.log(error));
-
-
+app.delete("/listings/:id", async (req, res) => {
+    const { id } = req.params;
+    await Listing.deleteOne({ _id: id });
+    res.redirect("/listings");
+})
 
 app.listen(port, () => {
     console.log(`Server started at port ${port}, http://localhost:${port}`);
